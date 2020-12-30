daa6d0c2fda10d1de2b2afc37f3d1fc145eeec94
Devansh3712 <devanshamity@gmail.com>
2020-12-30 14:00:54 +0530

    initial commit

---
+++ packages/conversion.py
@@ -0,0 +1,55 @@
+'''
+Module for taking user voice input
+and converting text to speech
+'''
+
+try:
+    import pyttsx3
+    import speech_recognition as sr
+
+except:
+    print('Speech Recognition and TTS not setup')
+    exit()
+
+#Class for taking user voice input
+class Listen:
+
+    def speech_to_content():
+
+        #Initializing Speech Recognition object
+        obj = sr.Recognizer()
+
+        with sr.Microphone() as source:
+
+            try:
+                audio     = obj.record(source, duration = 10)
+                converted = obj.recognize_google(audio)
+                return converted
+
+            except:
+                raise Exception('Error in Speech Recognition')
+
+#Class for converting text to speech
+class Speak:
+
+    def __init__(self, content):
+        self.content = content
+
+    def text_to_speech(self):
+
+        #Initialize Text to Speech object
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 150)
+
+        try:
+            #Convert the content to speech
+            engine.say(self.content)
+            engine.runAndWait()
+
+        except:
+            raise Exception('Error in Text to Speech')
+
+'''
+Devansh Singh, 2021
+GitHub: Devansh3712
+'''
